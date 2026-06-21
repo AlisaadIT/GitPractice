d618dc49e4ba82b57531b02ac7865546de04f585
Ali Saad <alisaad17018@gmail.com>
2026-06-21 16:11:06 +0300

    change code

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("Hello GitHub, this is Ali writing Python!")
+import random
+import string
+
+def generate_strong_password(length=16):
+   
+    characters = string.ascii_letters + string.digits + string.punctuation
+
+    password = ''.join(random.choice(characters) for i in range(length))
+    return password
+
+
+if __name__ == "__main__":
+    print("--- أداة توليد كلمات المرور المعقدة ---")
+    secure_pass = generate_strong_password(16)
+    print(f"تم توليد الرمز بنجاح: {secure_pass}")
